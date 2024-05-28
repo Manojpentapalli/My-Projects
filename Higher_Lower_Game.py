3d705b8a8b86d38b2e74890b75f46b00fd7cb8ba
Manoj Pentapalli <82868658+Manojpentapalli@users.noreply.github.com>
2024-05-28 12:05:31 +0530

    Add files via upload

---
+++ Higher_Lower_Game.py
@@ -0,0 +1,60 @@
+import random
+
+count=0 
+game=False
+go_again=False
+    
+f1= open("H_L_game_data.py",'r')
+from H_L_game_data import data
+# print(data)
+
+f2=open("H_L_game_art.py",'r')
+from H_L_game_art import logo, vs 
+
+while not go_again:
+    
+    print(logo)
+    
+    while not game:
+        
+        x= random.randint(0,49)
+        print(f'who do you think have more followers on instagram \nA - {data[x]["name"]}, a {data[x]["description"]}, from {data[x]["country"]}.')
+        
+        print(vs)
+        
+        y=random.randint(0,49)
+        if y==x:
+            y+=1 
+        print(f'or B - {data[y]["name"]}, a {data[y]["description"]}, from {data[y]["country"]}.')
+        
+        user=input("type A or B: ").lower()
+        
+        if user=="a":
+            if data[x]["follower_count"] > data[y]["follower_count"]:
+                count+=1 
+            else:
+                game=True
+                
+        elif user=="b":
+            if data[y]["follower_count"] > data[x]["follower_count"]:
+                count+=1 
+            else:
+                game=True
+                
+        else:
+            game=True
+    
+    print("wrong")        
+    print(f"your score: {count}")
+    
+    again=input("want to play again? type 'y' for yes and 'n' for no: ").lower()
+    if again=="y":
+        count=0
+        game=False
+        print("\033[H\033[J")
+    elif again=="n":
+        go_again=True 
+        
+        
+        
+        
